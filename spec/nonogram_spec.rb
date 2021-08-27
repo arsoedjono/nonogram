f7eb91884dd84fcc9667e9e2178ce3726ea59b91
arsoedjono <arsoedjono@gmail.com>
2021-08-27 16:09:07 +0700

    add initiate state spec

--- spec/nonogram_spec.rb
+++ spec/nonogram_spec.rb
@@ -1,11 +1,14 @@
 describe Nonogram do
   let(:rows) { [[3], [1,3], [3,1], [1,2], [1]] }
   let(:cols) { [[3], [1,1], [5], [2,1], [2]] }
+  let(:empty_solution) { Array.new(rows.size) { Array.new(cols.size, ' ') } }
+
+  subject(:nonogram) { described_class.new(rows, cols) }
+
+  its(:solution) { is_expected.to eq(empty_solution) }
 
   describe '#solve' do
-    subject { described_class.new(rows, cols).solve }
-
-    let(:empty_solution) { Array.new(rows.size) { Array.new(cols.size, ' ') } }
+    subject { nonogram.solve }
 
     its(:solution) { is_expected.not_to eq(empty_solution) }
   end
